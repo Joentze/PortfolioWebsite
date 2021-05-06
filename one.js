6ed10493fe37573432210d4924081fde63145e53
Joentze <Joentze01@gmail.com>
2021-05-06 02:51:41 -0700

    add carousel

--- one.js
+++ one.js
@@ -1,3 +1,7 @@
+let SetPosX;
+let SetPosY;
+let toggleButton = true;
+
 function portfolioButtonClick(){
     let container = document.getElementById('dropdown-container');
     console.log(container);
@@ -28,5 +32,33 @@ function slidingNav(){
         navBar.style.width = '0px'
         mainBody.style.marginLeft ='0px';
         navBar.style.paddingLeft = '0px'
+    }
+}
+
+
+function slidingCards(){
+    console.log('doing this')
+    let containerSlider = document.getElementById("containerSlider");
+    let carousel = document.getElementById('carousel')
+    let toggleArrow = document.getElementById('toggleArrow');
+    let textNarrative = document.getElementById("narrativeText");
+    let textNarrativeMarginLeft = getComputedStyle(textNarrative).marginLeft;
+    let maximumContainerLen = getComputedStyle(containerSlider).width;
+    if(toggleButton==true){
+        console.log('opening')
+        textNarrative.style.visibility = 'hidden';
+        carousel.style.width = maximumContainerLen;
+        toggleArrow.style.marginLeft = maximumContainerLen;
+        document.getElementById('thisbutton').style.backgroundImage = 'url(image/arrow-left-short.svg)';
+        toggleButton = false;
+        textNarrative.style.transitionDelay = '0.0s'
+    }
+    else if(toggleButton==false){
+        textNarrative.style.visibility = 'visible';
+        carousel.style.width = '50%';
+        toggleArrow.style.marginLeft = '49%';
+        textNarrative.style.transitionDelay = '0.8s'
+        document.getElementById('thisbutton').style.backgroundImage = 'url(image/arrow-right-short.svg)';
+        toggleButton = true;
     }
 }
